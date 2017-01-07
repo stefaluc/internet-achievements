a2b5495f22eea8c3734c019a903a4f44428ccee0
Lucas Stefanski <stefaluc000@gmail.com>
2017-01-06 22:18:44 -0600

    More changes to React.js

--- popup.js
+++ popup.js
@@ -1,9 +1,3 @@
-var cardContainer = document.getElementsByClassName('container')[1];
-
-window.onload = function() {
-    //init();
-}
-
 // reinitialize page when new achievement message is received
 chrome.runtime.onMessage.addListener(
     function(message, sender, sendResponse) {
@@ -21,10 +15,10 @@ chrome.runtime.onMessage.addListener(
                     }
                     chrome.notifications.create(opt);
                 }
+                location.reload();
             }, function(status) { // Promise rejected
                 alert('Error in xhr request, status of: ' + status);
             });
-            init();
         }
     }
 );
@@ -46,61 +40,56 @@ var getJSON = function(url) {
     });
 }
 
-function init() {
-    // get achievements.json data
-    getJSON('achievements.json').then(function(json) {
-        // clear dynamic content to reinit
-        cardContainer.innerHTML = '';
-        document.getElementById('points').innerHTML = '';
+var Main = React.createClass({
+    getInitialState: function() {
+        return { 
+            points: 0,
+            carouselItems: []
+        };
+    },
+    // function passed down to child to update points
+    updatePoints: function(e) {
+        this.setState({points: this.state.points + e});
+    },
 
-        var keys = Object.keys(json);
-        var points = 0;
-        // get gained achievements
-        chrome.storage.sync.get(keys, function(result) {
-            console.log(result[keys[0]]);
-            console.log(result);
-            cardContainer.innerHTML += '<div id="achieved" class="row"></div>';
-            cardContainer.innerHTML += '<div id="unachieved" class="row"></div>';
-            // display all achievements
-            for(var i = 0; i < keys.length; i++) {
-                if(result[keys[i]] && result[keys[i]].achieved) { // achieved
-                    console.log(json[keys[i]].points);
-                    points += json[keys[i]].points;
-                    document.getElementById('achieved').innerHTML += '<div class="col s12 m3"><div class="animated fadeIn card small white hoverable"><div class="card-content blue-text text-darken-1"><span class="card-title">' + json[keys[i]].title + '</span><p class="grey-text text-darken-3">' + json[keys[i]].description + '</p><p class="grey-text date">' + result[keys[i]].date + '</p></div><div class="card-action"><svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg><div class="points-card grey-text">' + json[keys[i]].points + ' pts</div></div></div></div>';
-                } else { // not achieved
-                    document.getElementById('unachieved').innerHTML += '<div class="col s12 m3"><div class="animated fadeIn card small grey lighten-3 hoverable"><div class="card-content blue-text text-darken-1"><span class="card-title">' + json[keys[i]].title + '</span><p class="grey-text text-darken-3">' + json[keys[i]].description + '</p></div><div class="card-action"><i class="animated rotateIn material-icons" style="font-size:40px; color:red">not_interested</i><div class="points-card grey-text">' + json[keys[i]].points + ' pts</div></div></div></div>';
-                }
-            }
-            document.getElementById('points').innerHTML += '<span class="right-align"><i class="material-icons yellow-text text-darken-1">star</i> Your Points: ' + points + '</span>';
+    render: function() {
+        return (
+            <div>
+                <div className="row">
+                    <div className="col s12 z-depth-1 white">
+                        <div className="container">
+                            <span id="title" className="blue-text text-darken-1">
+                                Internet Achievements
+                            </span>
+                            <div id="points" className="grey-text text-darken-3">
+                                <i className="material-icons yellow-text text-darken-1">star</i>
+                                Your Points: { this.state.points }
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-            // set cards to hidden and have them incrementally fade in
-            cards = document.getElementsByClassName('fadeIn');
-            fadeInCount = 0;
-            for (var i = 0; i < cards.length; i++) {
-                cards[i].className += ' hide';
-                setTimeout(fadeIn, 150*i);
-            }
-        });
-    }, function(status) { // Promise rejected
-        alert('Error in xhr request, status of: ' + status);
-    });
-}
+                <div className="container">
+                    <AchievementsList updateParent={ this.updatePoints } />
+                </div>
 
-var cards;
-var fadeInCount;
-function fadeIn() {
-    // remove hide class
-    cards[fadeInCount].className = cards[fadeInCount].className.slice(0, cards[fadeInCount].className.length - 5)
-    fadeInCount++;
-}
-
+                {/*<div id="modal1" className="modal">
+                    <div className="modal-content">
+                        <div className="carousel">
+                            { this.carouselItems }
+                        </div>
+                    </div>
+                </div>*/}
+            </div>
+        );
+    }
+});
 
 var AchievementsList = React.createClass({
     getInitialState: function() {
         return {
             achieved : [],
             unachieved: [],
-            points: 0
         }
     },
     
@@ -112,10 +101,6 @@ var AchievementsList = React.createClass({
         this.setState({unachieved: e});
     },
 
-    setPoints: function(e) {
-        this.setState({points: this.state.points + e});
-    },
-    
     // causes component to rerender when async call completes and this.state is updated
     componentDidMount: function() {
         getJSON('achievements.json').then(function(json) {
@@ -127,12 +112,10 @@ var AchievementsList = React.createClass({
                 for (var i = 0; i < keys.length; i++) {
                     if (result[keys[i]] && result[keys[i]].achieved) { // achieved
                         json[keys[i]].date = result[keys[i]].date; // copy date to json
-                        console.log(json[keys[i]]);
                         achieved.push(json[keys[i]]);
-                        this.setPoints(json[keys[i]].points);
+                        this.props.updateParent(json[keys[i]].points);
                     } else { // unachieved
                         unachieved.push(json[keys[i]]);
-                        this.setPoints(json[keys[i]].points);
                     }
                 }
                 // update state
@@ -143,13 +126,14 @@ var AchievementsList = React.createClass({
     },
 
     render: function() {
+        // only render when async complete
         if (this.state.achieved && this.state.unachieved) {
             // get achievement components
             var achievedComps = this.state.achieved.map(function(achievedObj, index) {
-                return <Achievement config={achievedObj} achieved="1" key={index} />;
+                return <Achievement config={achievedObj} achieved={1} key={index} />;
             });
             var unachievedComps = this.state.unachieved.map(function(unachievedObj, index) {
-                return <Achievement config={unachievedObj} achieved="" key={index} />;
+                return <Achievement config={unachievedObj} achieved={0} key={index} />;
             });
 
             return (
@@ -184,15 +168,12 @@ var Achievement = React.createClass({
                         {achieved ? (<p className="grey-text date">{ this.props.config.date }</p>) : ''}
                     </div>
                     <div className="card-action">
-                        {
-                            achieved 
-                                ? 
-                                <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
-                                    <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
-                                 </svg>
-                                :
-                                <i className="animated rotateIn material-icons ex">not_interested</i>
-                        }
+                        { achieved ? 
+                        <svg className="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                            <circle className="checkmark__circle" cx="26" cy="26" r="25" fill="none"/><path className="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+                        </svg>
+                        :
+                        <i className="animated rotateIn material-icons ex">not_interested</i> }
                         <div className="points-card grey-text">
                             { this.props.config.points }pts
                         </div>
@@ -203,5 +184,14 @@ var Achievement = React.createClass({
     }
 });
 
+// get the party started
+ReactDOM.render(
+    <Main />,
+    document.getElementById('main')
+);
 
-ReactDOM.render(<AchievementsList />, document.getElementById('achievements'));
+// initialize materialize js components
+$(document).ready(function() {
+    $('.carousel').carousel();
+    $('.modal').modal();
+});
